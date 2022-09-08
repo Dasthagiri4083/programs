763b58a465718ded0d5b21a4ca5586a240d6d380
Dasthagiri4083 <112654677+Dasthagiri4083@users.noreply.github.com>
2022-09-08 14:54:01 +0530

    Add files via upload

---
+++ ji.py
@@ -0,0 +1,11 @@
+def occurences(string1, string2):
+    count = 0
+    for word in string1:
+     for character in word:
+        count = count + 1
+    return count
+first_string = input('Enter the first string: ')
+second_string = input('Enter the second string: ')
+occurance_var = occurences(first_string,second_string)
+
+print(second_string,'ocures', occurance_var,'in', first_string)
